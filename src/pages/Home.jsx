b29ad43a35f988e0cd12732c236153ae954859ca
Sylvestor <145170477+cbbartlett@users.noreply.github.com>
2023-12-10 22:55:35 -0600

    Add React, Tailwind, and Vite config files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main className="container mx-auto p-4">
+      <h2 className="text-2xl font-bold mb-4">Welcome to My Showcase Website!</h2>
+      <p>This is a showcase of my work and projects.</p>
+    </main>
+  );
+};
+
+export default Home;
